245103738f22b1ce893908ca2d1f8aca060a19dd
Widy Agung <widy.agung@gmail.com>
2011-11-27 09:52:41 -0800

    second

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<html><head><title>Komuri</title></head><body background='#cecece'><style='color:#fff;'>Untuk project DJA ditaruh di sini...<br>Pake akses ssh atau git/svn commit atau pake filezilla yang sudah disediain</style><img src='http://statics.plurk.com/0efc4d55d28704f4370ef874ae906161.gif'></body></html>";
+?>
+
